275fe9c4a88ccebe81708f7b4262620eb79aa33a
gracehjy <g_huang21@yahoo.com>
2024-03-30 12:49:03 -0500

    update

--- chat-server.js
+++ chat-server.js
@@ -23,18 +23,44 @@ const socketio = require("socket.io")(http, {
     wsEngine: 'ws'
 });
 
-// let users = [];
-// let rooms = ("lobby",)
+// store rooms
+let rooms = [];
+
+// see if a room exists
+function roomExists(roomName) {
+    let exists = false;
+    rooms.forEach(room =>{
+        if(room === roomName){
+            exists = true;
+        }
+    });
+    return exists;
+}
+
+// add a new room
+function addRoom(roomName) {
+    if(!roomExists(roomName)){
+        rooms.push(roomName);
+    }
+}
 
 // Attach our Socket.IO server to our HTTP server to listen
 const io = socketio.listen(server);
 io.sockets.on("connection", function (socket) {
     // This callback runs when a new Socket.IO connection is established.
 
+    socket.on('join_room', function (data){
+        let {username, roomName, is_private, roomPassword} = data
+        socket.join(roomName);
+        
+        socket.to(roomName).emit('message_to_client', { message: `${username} has joined the room.` });
+        io.to(roomName).emit('room_users', { users: Object.keys(io.sockets.adapter.rooms[roomName].sockets)});
+    })
+
     socket.on('message_to_server', function (data) {
         // This callback runs when the server receives a new message from the client.
-
+        let {roomName, message } = data;
         console.log("message: " + data["message"]); // log it to the Node.JS output
-        io.sockets.emit("message_to_client", { message: data["message"] }) // broadcast the message to other users
+        io.to(roomName).emit("message_to_client", { message: message }) // broadcast the message to other users
     });
 });
