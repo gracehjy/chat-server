39c470a729a41e1612a0db1a0a7c8706d5a05715
gracehjy <g_huang21@yahoo.com>
2024-04-03 00:46:28 -0500

    Co-authored-by: johnpp4 <johnpp4@users.noreply.github.com>

--- README.md
+++ README.md
@@ -6,3 +6,20 @@ John Paul Pineda - 508919 - johnpp4
 
 ## Link
 http://ec2-100-27-27-36.compute-1.amazonaws.com:3456/client.html 
+
+## Required Functionality
+- Users can create chat rooms with an arbitrary room name
+- Users can join an arbitrary chat room
+- The chat room displays all users currently in the room
+- A private room can be created that is password protected
+- Creators of chat rooms can temporarily kick others out of the room 
+- Creators of chat rooms can permanently ban users from joining that particular room 
+- A user's message shows their username and is sent to everyone in the room
+- Users can send private messages to another user in the same room
+
+## Creative Portion
+- added a chatbot that notifys users of room events
+- added timestamps to every message
+- added options for users to select from a list of emojis to include in their messages
+- all users have unique usernames (have to refresh after the error alert tho in order to re-enter a username)
+- users are alerted when errors occur (e.g. username already taken, incorrect room password, room already created, etc.)
--- chat-server.js
+++ chat-server.js
@@ -1,16 +1,11 @@
-// Require the packages we will use:
-const http = require("http"),
-    fs = require("fs");
+const http = require("http");
+const fs = require("fs");
+const socketio = require("socket.io");
 
 const port = 3456;
-const file = "client.html";
-// Listen for HTTP connections.  This is essentially a miniature static file server that only serves our one file, client.html, on port 3456:
+const file = "client2.html";
 const server = http.createServer(function (req, res) {
-    // This callback runs when a new connection is made to our HTTP server.
-
     fs.readFile(file, function (err, data) {
-        // This callback runs when the client.html file has been read from the filesystem.
-
         if (err) return res.writeHead(500);
         res.writeHead(200);
         res.end(data);
@@ -18,49 +13,274 @@ const server = http.createServer(function (req, res) {
 });
 server.listen(port);
 
-// Import Socket.IO and pass our HTTP server object to it.
-const socketio = require("socket.io")(http, {
-    wsEngine: 'ws'
-});
+const io = socketio(server);
+const users = {};
+const rooms = { 'lobby': { isPrivate: false, members: [], password: '' } }; // initialize lobby as a default room
+const bannedUsers = {}; // um wip lol
+const roomOwner = {}; // also wip lol
+const kickedUsers = {};
 
-// store rooms
-let rooms = [];
+// ON CONNECTIONNNN do this pls
+io.on("connection", function (socket) {
 
-// see if a room exists
-function roomExists(roomName) {
-    let exists = false;
-    rooms.forEach(room =>{
-        if(room === roomName){
-            exists = true;
+     // make sure users can already see existing rooms
+     const roomList = getRoomList()
+     socket.emit('update_room_list', { rooms: roomList });
+
+    socket.on('join_lobby', ({username}) => {
+        // check if the username is already taken
+        if (Object.values(users).some(user => user.username === username)) {
+            socket.emit('error', { message: 'Username is already taken, please choose another one.' });
+            return;
+        }
+
+        // log user into the lobby
+        users[socket.id] = { username: username, room: 'lobby' };
+        socket.join('lobby');
+        updateusers('lobby');
+        socket.emit('room_joined', { room: 'lobby', isOwner: false});
+
+        console.log(`${username} has joined the lobby.`);
+        io.to('lobby').emit('message_to_client', {
+            username: 'ChatBot',
+            message: `: ${username} has joined the room.`
+        });
+    });
+
+    // let users pm each other
+    socket.on('private_message', function({ toUsername, message, fromUsername }) {
+        const recipientSocketId = Object.keys(users).find(key => users[key].username === toUsername);
+        const senderSocketId = Object.keys(users).find(key => users[key].username === fromUsername);
+        // if the user receiving exists, emit the message to their chat log
+        if (recipientSocketId) {
+            const timestamp = new Date().toLocaleTimeString();
+            const newMessage = ` [${timestamp}]: ${message}`;
+            io.to(recipientSocketId).emit('private_message_received', {
+                fromUsername: fromUsername, 
+                message: newMessage,
+            });
+            io.to(senderSocketId).emit('private_message_to', {
+                message: newMessage,
+                toUsername: toUsername
+            });
+        } else {
+            socket.emit('error', { message: `User ${toUsername} not found.` });
         }
     });
-    return exists;
-}
 
-// add a new room
-function addRoom(roomName) {
-    if(!roomExists(roomName)){
-        rooms.push(roomName);
-    }
-}
+    // event that a user joins a room
+    socket.on('join_room', ({ roomName, password }) => {
+        const user = users[socket.id];
+        const oldRoom = user.room;
+        let isOwner = determineIfOwner(socket.id, roomName); 
 
-// Attach our Socket.IO server to our HTTP server to listen
-const io = socketio.listen(server);
-io.sockets.on("connection", function (socket) {
-    // This callback runs when a new Socket.IO connection is established.
+        // if kicked, give alert
+        const now = new Date();
+        if (kickedUsers[roomName] && kickedUsers[roomName][username] && now < kickedUsers[roomName][username]) {
+            const kickedDurationLeft = (kickedUsers[roomName][username] - now) / 120; 
+            socket.emit('error', { message: `You are temporarily kicked from ${roomName}. Please wait ${kickedDurationLeft.toFixed(1)} more minutes.` });
+            return;
+        }
 
-    socket.on('join_room', function (data){
-        let {username, roomName, is_private, roomPassword} = data
-        socket.join(roomName);
+        // if banned, emit an error
+        if (bannedUsers[roomName] && bannedUsers[roomName].includes(username)) {
+            socket.emit('error', { message: 'You are banned from this room.'});
+            return;
+        }
+
+        // join/leave room logistics
+        if (rooms[roomName] && (!rooms[roomName].isPrivate || rooms[roomName].password === password)) {
+            socket.leave(oldRoom);
+            socket.join(roomName);
+            user.room = roomName;
+            updateusers(oldRoom);
+            updateusers(roomName);
+
+            console.log(`${user.username} has left ${oldRoom}.`)
+            console.log(`${user.username} has joined ${roomName}.`)
         
-        socket.to(roomName).emit('message_to_client', { message: `${username} has joined the room.` });
-        io.to(roomName).emit('room_users', { users: Object.keys(io.sockets.adapter.rooms[roomName].sockets)});
-    })
+            const userSocketId = Object.keys(users).find(key => users[key].username === user.username);
 
-    socket.on('message_to_server', function (data) {
-        // This callback runs when the server receives a new message from the client.
-        let {roomName, message } = data;
-        console.log("message: " + data["message"]); // log it to the Node.JS output
-        io.to(roomName).emit("message_to_client", { message: message }) // broadcast the message to other users
+            io.to(socket.id).emit('room_joined', { room: roomName, isOwner: roomOwner[roomName] === userSocketId});
+
+            // notify both rooms about the user
+            io.to(roomName).emit('message_to_client', {
+                username: 'ChatBot',
+                message: `: ${user.username} has joined the room.`
+            });
+            io.to(oldRoom).emit('message_to_client', {
+                username: 'ChatBot',
+                message: `: ${user.username} has left the room.`
+            });
+        } else {
+            socket.emit('error', { message: 'Invalid room name or password.'});
+        }
     });
+    
+    // event that a user creates a new room
+    socket.on('create_room', ({ roomName, isPrivate, password }) => {
+        // if room exists give an error
+        if (rooms[roomName]) {
+            socket.emit('error', { message: 'Room already exists'});
+        } else {
+            // create the room
+            rooms[roomName] = { isPrivate, password, members: [socket.id] };
+
+            // update the user's room
+            const user = users[socket.id];
+            const oldRoom = user.room;
+            user.room = roomName;
+            
+            // set roomOwner to the user's socket.id
+            roomOwner[roomName] = socket.id;
+ 
+            socket.leave(oldRoom);
+            socket.join(roomName);
+            const userSocketId = Object.keys(users).find(key => users[key].username === user.username);
+
+            io.to(socket.id).emit('room_joined', { room: roomName, isOwner: roomOwner[roomName] === userSocketId});
+
+            // notify the old room that the user has left
+            if (oldRoom) {
+                io.to(oldRoom).emit('message_to_client', {
+                    username: 'ChatBot',
+                    message: `: ${user.username} has left the room.`
+                });
+                updateusers(oldRoom);
+            }
+            updateRoomList();
+            // notify the new room that the user has joined
+            io.to(roomName).emit('message_to_client', {
+                username: 'ChatBot',
+                message: `: ${user.username} has joined the room.`
+            });
+            updateusers(roomName);
+
+            // server side console logs
+            console.log(`${user.username} has left ${oldRoom}.`)
+            console.log(`${user.username} has joined ${roomName}.`)
+        }
+    });
+
+    // event to kick a user
+    socket.on('kick_user', ({ usernameToKick, roomName }) => {
+        console.log("entered kick_user");
+        if (rooms[roomName] && roomOwner[roomName] === socket.id) {
+            const socketIdToKick = Object.keys(users).find(key => users[key].username === usernameToKick);
+            if (socketIdToKick) {
+                const kickedSocket = io.sockets.sockets.get(socketIdToKick);
+                if (kickedSocket) {
+                    kickedSocket.leave(roomName);
+    
+                    // users kicked are kicked for 60 seconds
+                    const duration = 60 * 1000;
+                    const kickDuration = new Date(new Date().getTime() + duration);
+    
+                    // record the kicked user
+                    if (!kickedUsers[roomName]) kickedUsers[roomName] = {};
+                    kickedUsers[roomName][usernameToKick] = kickDuration;
+    
+                    // notify the room that the user has been kicked
+                    io.to(roomName).emit('message_to_client', {
+                        username: 'ChatBot',
+                        message: `: ${usernameToKick} has been kicked from the room for 60 seconds.`,
+                    });
+                } else {
+                    console.log(`Error: Socket not found for user ${usernameToKick}`);
+                }
+            } else {
+                console.log(`Error: User ${usernameToKick} not found.`);
+            }
+        }
+    });
+    
+
+    // event to ban a user
+    socket.on('ban_user', ({ usernameToBan, roomName }) => {
+        console.log("entered ban_user");
+        if (rooms[roomName] && roomOwner[roomName] === socket.id) {
+            console.log("entered ban_user if 1");
+            if (!bannedUsers[roomName]) {
+                bannedUsers[roomName] = [];
+            }
+            const socketIdToBan = Object.keys(users).find(key => users[key].username === usernameToBan);
+            if (socketIdToBan) {
+                const banSocket = io.sockets.sockets.get(socketIdToBan);
+                if (banSocket) {
+                    banSocket.leave(roomName);
+                    // add the username to the list of banned users
+                    bannedUsers[roomName].push(usernameToBan);
+                    // notify the room that the user has been banned
+                    io.to(roomName).emit('message_to_client', {
+                        username: 'ChatBot',
+                        message: `: ${usernameToBan} has been banned from the room.`,
+                    });
+                } else {
+                    console.log(`Error: Socket not found for user ${usernameToBan}`);
+                }
+            } else {
+                console.log(`Error: User ${usernameToBan} not found.`);
+            }
+        }
+    });
+
+
+    // broadcast the message to users (in room vs lobby)
+    socket.on('message_to_server', function ({ message, roomName }) {
+        console.log("message: " + message);
+
+        // omg we added a timestamp!!!!
+        const timestamp = new Date().toLocaleTimeString();
+        const newMessage = ` [${timestamp}]: ${message}`;
+        io.to(roomName).emit("message_to_client", { username: users[socket.id].username, message: newMessage });
+    });
+
+    // when a user disconnects from the page, remove the user from the user list
+    socket.on('disconnect', function () {
+        const user = users[socket.id];
+        if (user) { 
+            const roomName = user.room;
+            if (rooms[roomName]) {
+                rooms[roomName].members = rooms[roomName].members.filter(memberId => memberId !== socket.id);
+                // notify the room that a user has left
+                io.to(roomName).emit('message_to_client', {
+                    username: 'ChatBot',
+                    message: `: ${user.username} has left the room.`
+                });
+                updateusers(roomName);
+            }
+            delete users[socket.id]; 
+        }
+    });
+
+    function determineIfOwner(socketId, roomName) {
+        return roomOwner[roomName] === socketId;
+    }
+
+    // update the list of users in the lobby
+    function updateusers(room) {
+        const roomUsers = Object.values(users)
+            .filter(user => user.room === room)
+            .map(user => user.username);
+        io.to(room).emit('lobby_user_list', { users: roomUsers });
+    }
+
+    // update the list of rooms existings
+    function updateRoomList() {
+        const roomList = Object.keys(rooms).map(roomName => ({
+            name: roomName,
+            isPrivate: rooms[roomName].isPrivate
+        }));
+        io.emit('update_room_list', { rooms: roomList });
+    }
+
+    // get the roomlist
+    function getRoomList() {
+        return Object.keys(rooms).map(roomName => {
+            return {
+                name: roomName,
+                isPrivate: rooms[roomName].isPrivate
+            };
+        });
+    }
 });
